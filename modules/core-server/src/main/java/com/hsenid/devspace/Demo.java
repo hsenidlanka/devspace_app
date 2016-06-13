b26d3b49b7dae3e2c672ae17232f517285d67734
kasundm5566 <kasundinesh5566@gmail.com>
2016-06-13 15:03:13 +0530

    Updated multi module structure.

---
+++ modules/core-server/src/main/java/com/hsenid/devspace/Demo.java
@@ -0,0 +1,11 @@
+package com.hsenid.devspace;
+
+/**
+ * Created by hsenid on 6/13/16.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
