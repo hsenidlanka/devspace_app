3b1dd4f07103108cd5c63620de861a9630806839
kasundm5566 <kasundinesh5566@gmail.com>
2016-06-28 14:33:44 +0530

    Configured Java Service Wrapper and Maven Assembly plugin.

--- modules/core-server/src/main/java/com/hsenid/devspace/Demo.java
+++ modules/core-server/src/main/java/com/hsenid/devspace/Demo.java
@@ -1,11 +1,47 @@
 package com.hsenid.devspace;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.tanukisoftware.wrapper.WrapperListener;
+import org.tanukisoftware.wrapper.WrapperManager;
+
+import java.net.MalformedURLException;
+
 /**
  * Created by hsenid on 6/13/16.
  */
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Demo implements WrapperListener {
+    private static final Logger log = LogManager.getLogger(Demo.class);
 
+    @Override
+    public Integer start(String[] strings) {
+        try {
+            log.info("Core server is starting...");
+            System.out.println("Hello World!");
+            log.info("Core server started...");
+        } catch (Exception e) {
+            log.error("Error occured. " + e);
+        }
+        return null;
+    }
+
+    @Override
+    public int stop(int i) {
+        log.info("Core server stopped...");
+        return 0;
+    }
+
+    @Override
+    public void controlEvent(int i) {
+        if ((i == WrapperManager.WRAPPER_CTRL_LOGOFF_EVENT)
+                && WrapperManager.isLaunchedAsService()) {
+            // Ignore
+        } else {
+            WrapperManager.stop(0);
+        }
+    }
+
+    public static void main(String[] args) throws MalformedURLException {
+        WrapperManager.start(new Demo(), args);
     }
 }
