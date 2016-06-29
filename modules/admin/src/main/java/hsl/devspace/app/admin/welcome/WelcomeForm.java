cd47644ba0adc2d9c53d87bbc78273538580fc35
difnajayashani <difna4@gmail.com>
2016-06-29 16:57:03 +0530

    Welcome page Controller class and log4j.xml added

--- modules/admin/src/main/java/hsl/devspace/app/admin/welcome/WelcomeForm.java
+++ modules/admin/src/main/java/hsl/devspace/app/admin/welcome/WelcomeForm.java
@@ -1,9 +1,44 @@
 package hsl.devspace.app.admin.welcome;
 
-/**
- * Created by hsenid on 6/28/16.
- */
 public class WelcomeForm {
+
+        private int pendingNotifications;
+        private int pendingRewards;
+        private int pendingCampaigns;
+        private int pendingProfiles;
+
+        public int getPendingNotifications() {
+            return pendingNotifications;
+        }
+
+        public void setPendingNotifications(int pendingNotifications) {
+            this.pendingNotifications = pendingNotifications;
+        }
+
+        public int getPendingRewards() {
+            return pendingRewards;
+        }
+
+        public void setPendingRewards(int pendingRewards) {
+            this.pendingRewards = pendingRewards;
+        }
+
+        public int getPendingCampaigns() {
+            return pendingCampaigns;
+        }
+
+        public void setPendingCampaigns(int pendingCampaigns) {
+            this.pendingCampaigns = pendingCampaigns;
+        }
+
+        public int getPendingProfiles() {
+            return pendingProfiles;
+        }
+
+        public void setPendingProfiles(int pendingProfiles) {
+            this.pendingProfiles = pendingProfiles;
+        }
+
 
 
 }
