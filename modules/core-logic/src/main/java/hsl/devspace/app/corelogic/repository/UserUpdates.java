ec8711fc15b2611f76b4fd60a32fa22bdb2eb991
Dineesha <dinsuraw@gmail.com>
2016-07-05 14:58:41 +0530

    Implement method for delete user

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/UserUpdates.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/UserUpdates.java
@@ -4,7 +4,7 @@ package hsl.devspace.app.corelogic.repository;
  * Created by hsenid on 7/5/16.
  */
 public interface UserUpdates {
-    int deleteUser();
+    void deleteUser(String username);
 
     String changePassword();
 
--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/UserUpdatesImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/UserUpdatesImpl.java
@@ -1,14 +1,41 @@
 package hsl.devspace.app.corelogic.repository;
 
 import hsl.devspace.app.corelogic.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
 
 /**
  * Created by hsenid on 7/5/16.
  */
 public class UserUpdatesImpl implements UserUpdates {
     User user=new User();
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+    private PlatformTransactionManager transactionManager;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    public void setTransactionManager(PlatformTransactionManager transactionManager){
+        this.transactionManager=transactionManager;
+    }
     @Override
-    public int deleteUser() { return user.getUsr_id(); }
+    public void deleteUser(String username) {
+        TransactionDefinition tr_def = new DefaultTransactionDefinition();
+        TransactionStatus stat = transactionManager.getTransaction(tr_def);
+        user.setUsername(username);
+        String sql = "DELETE FROM users WHERE username = ?";
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        jdbcTemplate.update(sql, new Object[]{ user.getUsername() });
+        transactionManager.commit(stat);
+    }
 
     @Override
     public String changePassword() {
