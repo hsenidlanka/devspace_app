e8963808cbc80e06247d2aeec03bb660928034e5
Menuka <menukaishan5@gmail.com>
2016-08-11 17:24:01 +0530

    Updating Documentation with Mind map, Sequence diagrams and Mockups

---
+++ docs/requirement - Mind Mad/Pizza Shefu.mm
@@ -0,0 +1,219 @@
+
+<map version="0.9.0">
+    <node TEXT="**Pizza Shefu**" FOLDED="false">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="17" />
+        <node TEXT="Feedback" FOLDED="false" POSITION="left">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Hot lines" FOLDED="false">
+                <edge COLOR="#60d6d0" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Contact Us" FOLDED="false">
+                <edge COLOR="#60d2c9" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Review and ratings  " FOLDED="false">
+                <edge COLOR="#68d6af" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Login:" FOLDED="false" POSITION="left">
+            <edge COLOR="#efa670" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Open Access" FOLDED="false">
+                <edge COLOR="#ef9f6e" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Guest Access" FOLDED="false">
+                <edge COLOR="#f19278" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="User Login" FOLDED="false">
+                <edge COLOR="#efaa6c" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Selections" FOLDED="false" POSITION="right">
+            <edge COLOR="#988ee3" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Packages" FOLDED="false">
+                <edge COLOR="#a58fe4" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Business Packages" FOLDED="false">
+                    <edge COLOR="#9a84e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Party Packages" FOLDED="false">
+                    <edge COLOR="#b38be4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Kids Packages" FOLDED="false">
+                    <edge COLOR="#8e85e2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Pizza" FOLDED="false">
+                <edge COLOR="#8d8de2" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Pan and Other Crusts" FOLDED="false">
+                    <edge COLOR="#989fe3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Big Pizza" FOLDED="false">
+                    <edge COLOR="#9092e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Italiano" FOLDED="false">
+                    <edge COLOR="#a197e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Pasta and Sides" FOLDED="false">
+                <edge COLOR="#ac96e3" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Rice" FOLDED="false">
+                    <edge COLOR="#ac9ce5" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Appertizer" FOLDED="false">
+                    <edge COLOR="#ae99e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Pasta" FOLDED="false">
+                    <edge COLOR="#ad8ee1" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Desserts" FOLDED="false">
+                    <edge COLOR="#a88fe0" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Beverages" FOLDED="false">
+                    <edge COLOR="#ae90e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Promos" FOLDED="false">
+                <edge COLOR="#968ce1" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Seasonal" FOLDED="false">
+                    <edge COLOR="#8e83e0" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Occasional  " FOLDED="false">
+                    <edge COLOR="#8987e2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Loyalty Promos" FOLDED="false">
+                    <edge COLOR="#9986e1" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Events:green_cross:" FOLDED="false" POSITION="left">
+            <edge COLOR="#e68782" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Occasional Events" FOLDED="false">
+                <edge COLOR="#9b8ee1" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Contest" FOLDED="false">
+                <edge COLOR="#9583e2" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Seasonal Events" FOLDED="false">
+                <edge COLOR="#8b94e4" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Payment" FOLDED="false" POSITION="left">
+            <edge COLOR="#efa670" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Online" FOLDED="false">
+                <edge COLOR="#eeaa67" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Pay on delivery" FOLDED="false">
+                <edge COLOR="#f0b56f" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Cash " FOLDED="false">
+                    <edge COLOR="#efc770" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Cards" FOLDED="false">
+                    <edge COLOR="#f2ab77" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Ordering" FOLDED="false" POSITION="right">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Online Purchasing " FOLDED="false">
+                <edge COLOR="#63d9b7" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="On call" FOLDED="false">
+                <edge COLOR="#5fd3d5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Billing with service Charges" FOLDED="false" POSITION="left">
+            <edge COLOR="#e68782" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Discounted " FOLDED="false">
+                <edge COLOR="#e78d83" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Coupons" FOLDED="false">
+                    <edge COLOR="#e4857a" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Seasonal discounts" FOLDED="false">
+                    <edge COLOR="#e7827f" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Without discount " FOLDED="false">
+                <edge COLOR="#e79d83" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="News Alert:green_cross:" FOLDED="false" POSITION="right">
+            <edge COLOR="#e096e9" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Registered Users" FOLDED="false">
+                <edge COLOR="#e692e7" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Subcriptions" FOLDED="false">
+                <edge COLOR="#e98ee6" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Users" FOLDED="false" POSITION="right">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Guest User" FOLDED="false">
+                <edge COLOR="#759ae2" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Registered Users" FOLDED="false">
+                <edge COLOR="#76a8e5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Delivery" FOLDED="false" POSITION="right">
+            <edge COLOR="#ebd95f" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Pick up" FOLDED="false">
+                <edge COLOR="#8ad662" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Door step" FOLDED="false">
+                <edge COLOR="#acd064" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+    </node>
+</map>
