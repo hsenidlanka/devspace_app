e5eadfe53073cd02706285e0345823162382eba2
difnajayashani <difna4@gmail.com>
2016-08-25 18:00:48 +0530

    user view page completed

---
+++ modules/ui/admin/js/customer_table.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+            $('#table').bootstrapTable({
+
+                url: '../js/customer.json',
+                height: 420,
+                pagination: true,
+                pageSize: 8,
+                clickToSelect: true,
+                singleSelect: true,
+                minimumCountColumns: 3,
+                columns: [{
+                    //field: 'id',
+                    field:'ID',
+                    title: 'ID :',
+                    sortable: true,
+                    align:'left'
+                },{
+                    field: 'Username',
+                    title: 'Username :',
+                    sortable: true,
+                    align:'left',
+                    searchable:true
+                }, {
+                    field: 'f_name',
+                    title: 'First Name :',
+                    align:'left',
+                    sortable: true
+                }, {
+                    field: 'l_name',
+                    title: 'Last Name :',
+                    align:'left',
+                    sortable: true
+                }, {
+                    field: 'mobile',
+                    title: 'Phone No :',
+                    align:'left'
+                }, {
+                    field: 'e_mail',
+                    title: 'Email :',
+                    align:'left'
+                }, {
+                    field: 'address',
+                    title: 'Address :',
+                    align:'left'
+                }, {
+                    field: 'Options',
+                    title: 'Options :',
+                    align: 'center',
+                    formatter: operateFormatter,
+                    events: operateEvents
+                }]
+            });
+})
+
+function operateFormatter(value, row, index) {
+    return [
+        '<a class="like" href="javascript:void(0)" title="Like" >',
+        '<em class="fa fa-pencil"></em>',
+        '</a>  ',
+        '<a class="remove" href="javascript:void(0)" title="Delete">',
+        '<i class="glyphicon glyphicon-remove"></i>',
+        '</a>'
+    ].join('');
+}
+
+window.operateEvents = {
+    'click .like': function(){
+          $('#editModal').modal('show');
+
+          //alert('You click like action, row: ' );
+    },
+
+    'click .remove': function () {
+        $('#removeModal').modal({show:true});
+
+        //alert('You click remove action, row: ');
+
+    }
+}
--- modules/ui/admin/js/userMgt.js
+++ modules/ui/admin/js/userMgt.js
@@ -1,15 +1,83 @@
 
+//datepicker
+$(document).ready(function() {
+    $("#fromDate").datepicker({
+        "changeMonth":true,
+        "changeYear":true
+
+    });
+
+    $("#toDate").datepicker({
+        "changeMonth":true,
+        "changeYear":true
+
+    });
+});
+
+//userAdd js
 $(document).ready(function () {
     $("#staffDetail").hide();
 
     $("#staff").click(function(){
         $("#staffDetail").show();
-
     });
-    $("#customer").click(function(){
 
+    $("#customer").click(function(){
         $("#staffDetail").hide();
     });
 
 });
+
+
+//usersView js
+
+//function to populate the filter by criteria
+function configureDropDownLists(ddl1,ddl2) {
+    var customer= ['None','Name', 'Registered Period', 'Area'];
+    var staff = ['None','Designation', 'Department', 'Branch'];
+
+
+    switch (ddl1.value) {
+        case 'Customer':
+            ddl2.options.length = 0;
+            for (i = 0; i < customer.length; i++) {
+                createOption(ddl2, customer[i], customer[i]);
+            }
+            break;
+        case 'Staff Member':
+            ddl2.options.length = 0;
+            for (i = 0; i < staff.length; i++) {
+                createOption(ddl2, staff[i], staff[i]);
+            }
+            break;
+
+        default:
+            ddl2.options.length = 0;
+            break;
+    }
+
+}
+
+function createOption(ddl, text, value) {
+    var opt = document.createElement('option');
+    opt.value = value;
+    opt.text = text;
+    ddl.options.add(opt);
+}
+
+
+
+    function showMe(e){
+        var status= e.options[e.selectedIndex].value;
+        var regPeriod= document.getElementById("regPeriod");
+        var fname= document.getElementById("name");
+
+        if (status == "Registered Period"){
+            regPeriod.style.display = "block";
+        } else{
+            regPeriod.style.display = "none";
+        }
+    }
+
+
 
