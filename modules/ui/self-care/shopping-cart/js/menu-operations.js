472291967a1f11786a1dedb5a02029a3090da8d1
kasundm5566 <kasundinesh5566@gmail.com>
2016-08-26 12:35:11 +0530

    Applied modifications to the Menu page.

--- modules/ui/self-care/shopping-cart/js/menu-operations.js
+++ modules/ui/self-care/shopping-cart/js/menu-operations.js
@@ -1,19 +1,39 @@
 $(document).ready(function () {
-    $('.panel-heading a').on('click', function (e) {
+    $("#desserts-collapse").hide(menuThumbnailsOverlay());
+    $("#beverage-collapse").hide(menuThumbnailsOverlay());
+
+    // Keep only one collapsible group open.
+/*    $('.panel-heading a').on('click', function (e) {
         if ($(this).parents('.panel').children('.panel-collapse').hasClass('in')) {
             e.stopPropagation();
-            $('.menu-images').contenthover({
-                overlay_background: '#000',
-                overlay_opacity: 0.8,
-                overlay_y_position: 'center'
-            });
         }
+    });*/
+
+    menuThumbnailsOverlay();
+
+    $(".tab-desserts").click(function () {
+        $("#pizza-collapse").hide();
+        $("#beverage-collapse").hide();
+        $("#desserts-collapse").show();
     });
 
+    $(".tab-pizzas").click(function () {
+        $("#desserts-collapse").hide();
+        $("#beverage-collapse").hide();
+        $("#pizza-collapse").show();
+    });
+
+    $(".tab-beverages").click(function(){
+        $("#desserts-collapse").hide();
+        $("#pizza-collapse").hide();
+        $("#beverage-collapse").show();
+    });
+});
+
+function menuThumbnailsOverlay() {
     $('.menu-images').contenthover({
         overlay_background: '#000',
         overlay_opacity: 0.8,
         overlay_y_position: 'center'
     });
-
-});
+}
