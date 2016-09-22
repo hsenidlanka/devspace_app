9120b2a67bc2e94d3414531d0a0b0d51fc60b068
Dineesha <dinsuraw@gmail.com>
2016-09-22 16:17:11 +0530

    Update transaction manager

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/coupon/CouponRepositoryImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/coupon/CouponRepositoryImpl.java
@@ -4,9 +4,6 @@ import hsl.devspace.app.corelogic.domain.Coupon;
 import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -17,13 +14,11 @@ import java.util.Map;
  */
 public class CouponRepositoryImpl implements CouponRepository {
     Coupon coupon=new Coupon();
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
     private PlatformTransactionManager transactionManager;
     private static org.apache.log4j.Logger log = Logger.getLogger(CouponRepositoryImpl.class);
 
     public void setDataSource(DataSource dataSource) {
-        //this.dataSource = dataSource;
         jdbcTemplate = new JdbcTemplate(dataSource);
 
     }
@@ -34,15 +29,10 @@ public class CouponRepositoryImpl implements CouponRepository {
     }
     @Override
     public int add(String couponCode,String customerMobile){
-
         int row;
-        TransactionDefinition tr_def = new DefaultTransactionDefinition();
-        TransactionStatus stat = transactionManager.getTransaction(tr_def);
-
         String sql = "INSERT INTO coupon" +
                 "(coupon_code,rate,expire_date,status,customer_mobile) VALUES (?,10,DATE_ADD(CURRENT_DATE,INTERVAL 30 DAY),1,?)";
         row = jdbcTemplate.update(sql, new Object[]{couponCode, customerMobile});
-        transactionManager.commit(stat);
         log.info(row + "new coupon inserted");
         return row;
     }
@@ -50,11 +40,8 @@ public class CouponRepositoryImpl implements CouponRepository {
     @Override
     public int expire() {
         int row;
-        TransactionDefinition tr_def = new DefaultTransactionDefinition();
-        TransactionStatus stat = transactionManager.getTransaction(tr_def);
         String sql="UPDATE coupon SET status=3 WHERE expire_date <=CURRENT_DATE";
         row=jdbcTemplate.update(sql);
-        transactionManager.commit(stat);
         log.info("expired");
         return row;
     }
--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/delivery/DeliveryRepositoryImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/delivery/DeliveryRepositoryImpl.java
@@ -4,9 +4,6 @@ import hsl.devspace.app.corelogic.domain.Delivery;
 import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
 
@@ -15,13 +12,11 @@ import javax.sql.DataSource;
  */
 public class DeliveryRepositoryImpl implements DeliveryRepository {
     Delivery del=new Delivery();
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
     private PlatformTransactionManager transactionManager;
     private static org.apache.log4j.Logger log = Logger.getLogger(DeliveryRepositoryImpl.class);
 
     public void setDataSource(DataSource dataSource) {
-        //this.dataSource = dataSource;
         jdbcTemplate = new JdbcTemplate(dataSource);
 
     }
@@ -34,26 +29,20 @@ public class DeliveryRepositoryImpl implements DeliveryRepository {
     @Override
     public int add(Delivery delivery) {
         int row;
-        TransactionDefinition tr_def = new DefaultTransactionDefinition();
-        TransactionStatus stat = transactionManager.getTransaction(tr_def);
         String sql = "INSERT INTO delivery " +
                 "(agent_name,recepient_name,recepient_address,delivery_date,delivery_time,delivery_status,description,staff_id,delivery_method_id) VALUES (?,?,?,?,?,?,?,(SELECT id FROM staff WHERE name=?),(SELECT id FROM delivery_method WHERE name=?))";
 
         row = jdbcTemplate.update(sql, new Object[]{del.getAgentName(), del.getRecepientName(), del.getRecepientAddress(),
                 del.getDeliveryDate(), del.getDeliveryTime(), del.getDeliveryStatus(), del.getDescription(), del.getAgentName(), del.getDeliveryMethod()});
-        transactionManager.commit(stat);
         log.info(row + "new delivery inserted");
         return row;
     }
 
     @Override
     public int changeStatus(int id) {
-        TransactionDefinition tr_def = new DefaultTransactionDefinition();
-        TransactionStatus stat = transactionManager.getTransaction(tr_def);
 
         String sql = "UPDATE delivery SET delivery_status='Delivered' WHERE id = ?";
         int row = jdbcTemplate.update(sql, new Object[]{id});
-        transactionManager.commit(stat);
         log.info(row + "delivered");
         return row;
     }
@@ -61,13 +50,10 @@ public class DeliveryRepositoryImpl implements DeliveryRepository {
     @Override
     public int addDeliveryMethod(String deliveryMethod) {
         int row;
-        TransactionDefinition tr_def = new DefaultTransactionDefinition();
-        TransactionStatus stat = transactionManager.getTransaction(tr_def);
         String sql = "INSERT INTO delivery_method " +
                 "(name) VALUES (?)";
 
         row = jdbcTemplate.update(sql, new Object[]{deliveryMethod});
-        transactionManager.commit(stat);
         log.info(row + "new delivery method inserted");
         return row;
     }
@@ -75,12 +61,9 @@ public class DeliveryRepositoryImpl implements DeliveryRepository {
     @Override
     public int deleteDeliveryMethod(String deliveryMethod) {
         int row;
-        TransactionDefinition tr_def = new DefaultTransactionDefinition();
-        TransactionStatus stat = transactionManager.getTransaction(tr_def);
-        String sql = "DELETE FROM delivery_method WHERE name=?";
 
+        String sql = "DELETE FROM delivery_method WHERE name=?";
         row = jdbcTemplate.update(sql, new Object[]{deliveryMethod});
-        transactionManager.commit(stat);
         log.info(row + " delivery method deleted");
         return row;
     }
