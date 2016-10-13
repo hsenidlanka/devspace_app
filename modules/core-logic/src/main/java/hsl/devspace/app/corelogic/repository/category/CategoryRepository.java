e56957b35b19f24db153eb6e8a500b29812ca4d0
Dineesha <dinsuraw@gmail.com>
2016-10-13 08:12:57 +0530

    Refactor return types to Category object

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/category/CategoryRepository.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/category/CategoryRepository.java
@@ -1,11 +1,11 @@
 package hsl.devspace.app.corelogic.repository.category;
 
 import hsl.devspace.app.corelogic.domain.Category;
+import hsl.devspace.app.corelogic.domain.Item;
 import org.springframework.dao.DuplicateKeyException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by hsenid on 9/15/16.
@@ -15,16 +15,16 @@ public interface CategoryRepository {
     public int add(Category category) throws SQLIntegrityConstraintViolationException, DuplicateKeyException;
     boolean checkAvailability(String categoryName);
     public int delete(String categoryName);
-    List<Map<String, Object>> view();
+    List<Category> selectAll();
     int count();
     int update(String name,String description);
-    List<Map<String, Object>> viewList();
+    List<Category> selectNameAndDescription();
     void createCategory1(Category cat);
-    List<Map<String, Object>> retrieveCategoryTypes(String categoryName);
-    List<Map<String, Object>> loadMenuItems(String catName);
-    List<Map<String, Object>> viewSubCategories(String catName);
-    List<Map<String, Object>> viewCategoryList();
-    List<Map<String, Object>>  retrieveSubcatogories(String category);
+    List<String> retrieveCategoryTypes(String categoryName);
+    List<Item> loadMenuItems(String catName);
+    List<String> viewSubCategories(String catName);
+    List<String> selectCategoryNames();
+    List<String>  retrieveSubcatogories(String category);
 
 
 
--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/category/SubCategoryRepositoryImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/category/SubCategoryRepositoryImpl.java
@@ -1,7 +1,8 @@
 package hsl.devspace.app.corelogic.repository.category;
 
 import hsl.devspace.app.corelogic.domain.Category;
-import org.apache.log4j.Logger;
+import hsl.devspace.app.corelogic.domain.Item;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
@@ -9,6 +10,7 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +21,8 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
     Category category = new Category();
     private JdbcTemplate jdbcTemplate;
     private PlatformTransactionManager transactionManager;
-    private static org.apache.log4j.Logger log = Logger.getLogger(SubCategoryRepositoryImpl.class);
+   // private static org.apache.log4j.Logger log = Logger.getLogger(SubCategoryRepositoryImpl.class);
+   org.slf4j.Logger log = LoggerFactory.getLogger(SubCategoryRepositoryImpl.class);
 
     public void setDataSource(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
@@ -43,9 +46,9 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
             String sql = "INSERT INTO sub_category " +
                     "(name,description,creator,category_id) VALUES (?,?,?,(SELECT id FROM category WHERE name=?))";
             row = jdbcTemplate.update(sql, new Object[]{category.getSubCategoryName(), category.getDescription(), category.getCreator(), category.getCategoryName()});
-            log.info(row + "new sub category inserted");
+            log.info("{} new sub category inserted",row);
         } else
-            log.info(row + "sub category already available");
+            log.info("{} sub category already available",row);
 
         return row;
     }
@@ -57,13 +60,13 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
         boolean result;
 
         List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM sub_category WHERE  name=?", subCategoryName);
-        log.info(mp);
+        log.info("{}",mp);
 
         if (mp.size() != 0) {
             result = true;
         } else
             result = false;
-        log.info(result);
+        log.info("{}",result);
         return result;
     }
 
@@ -75,16 +78,31 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
 
         String sql = "DELETE FROM sub_category WHERE name = ?";
         int row = jdbcTemplate.update(sql, new Object[]{category.getSubCategoryName()});
-        log.info(row + " sub category deleted");
+        log.info("{} sub category deleted",row);
         return row;
     }
 
     /*view all details of subcategory*/
     @Override
-    public List<Map<String, Object>> view() {
+    public List<Category> selectAll() {
         List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM sub_category");
-        log.info(mp);
-        return mp;
+        List<Category> subCategories=new ArrayList<Category>();
+
+        for (int i=0;i<mp.size();i++){
+            //for(int j=0;j<mp.get(i).size();j++) {
+                Category category = new Category();
+                category.setSubCategoryId(Integer.parseInt(mp.get(i).get("id").toString()));
+                category.setSubCategoryName(mp.get(i).get("name").toString());
+                category.setDescription(mp.get(i).get("description").toString());
+                category.setCreator(mp.get(i).get("creator").toString());
+                category.setCategory_id(Integer.parseInt(mp.get(i).get("category_id").toString()));
+
+                subCategories.add(category);
+           // }
+
+        }
+        log.info("{}",subCategories);
+        return subCategories;
     }
 
     /*retrieve total no.of subcategories*/
@@ -92,7 +110,7 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
     public int count() {
         List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM sub_category ");
         int count = mp.size();
-        log.info(count);
+        log.info("{}",count);
         return count;
     }
 
@@ -104,18 +122,33 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
 
     /*retrieve all subcategory names*/
     @Override
-    public List<Map<String, Object>> viewList() {
-        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT name FROM sub_category");
-        log.info(mp);
-        return mp;
+    public List<Category> selectNameAndDescription() {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT name,description FROM sub_category");
+        List<Category> subCategories=new ArrayList<Category>();
+
+        for (int i=0;i<mp.size();i++){
+            Category category = new Category();
+            category.setSubCategoryName(mp.get(i).get("name").toString());
+            category.setDescription(mp.get(i).get("description").toString());
+            subCategories.add(category);
+
+
+        }
+        log.info("{}",subCategories);
+        return subCategories;
     }
 
     /*retrieve subcategory names list in a specific category*/
     @Override
-    public List<Map<String, Object>>  retrieveSubcatogories(String category){
+    public List<String>  retrieveSubcatogories(String category){
         List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT name FROM sub_category WHERE category_id=(SELECT id FROM category WHERE NAME =?)",category);
-        log.info(mp);
-        return mp;
+        List<String> subCatName=new ArrayList<String>();
+        for (int i=0;i<mp.size();i++){
+            String nm=mp.get(i).get("name").toString();
+            subCatName.add(nm);
+        }
+        log.info("{}",subCatName);
+        return subCatName;
     }
 
 
@@ -134,26 +167,41 @@ public class SubCategoryRepositoryImpl implements CategoryRepository {
     }
 
     @Override
-    public List<Map<String, Object>> retrieveCategoryTypes(String categoryName) {
+    public List<String> retrieveCategoryTypes(String categoryName) {
         return null;
 
     }
 
     /*retrieve all items in a specific subcategory*/
     @Override
-    public List<Map<String, Object>> loadMenuItems(String subCatName) {
-        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM item WHERE sub_category_id=(SELECT id FROM sub_category WHERE name=?)",subCatName);
-        log.info(mp);
-        return mp;
+    public List<Item> loadMenuItems(String subCatName) {
+        List<Map<String, Object>> another = jdbcTemplate.queryForList("SELECT * FROM item WHERE sub_category_id=(SELECT id FROM sub_category WHERE name=?)",subCatName);
+        List<Item> itemList=new ArrayList<Item>();
+
+        log.info("{}",another);
+        for (int j=0;j<another.size();j++){
+            Item item=new Item();
+            item.setItemId(Integer.parseInt(another.get(j).get("id").toString()));
+            item.setItemName(another.get(j).get("name").toString());
+            item.setDescription(another.get(j).get("description").toString());
+            item.setTypeId(Integer.parseInt(another.get(j).get("type_id").toString()));
+            item.setImage(another.get(j).get("image").toString());
+            item.setSubCategoryId(Integer.parseInt(another.get(j).get("sub_category_id").toString()));
+            itemList.add(item);
+
+
+        }
+        log.info("{}",itemList);
+        return itemList;
     }
 
     @Override
-    public List<Map<String, Object>> viewSubCategories(String catName) {
+    public List<String> viewSubCategories(String catName) {
         return null;
     }
 
     @Override
-    public List<Map<String, Object>> viewCategoryList() {
+    public List<String> selectCategoryNames() {
         return null;
     }
 }
