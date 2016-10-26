9c05fa348488da52f074118e85c494332c458b1e
Dineesha <dinsuraw@gmail.com>
2016-10-26 15:00:32 +0530

    Add new select methods

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/contact_us/ContactUsRepositoryImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/contact_us/ContactUsRepositoryImpl.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -42,14 +43,92 @@ public class ContactUsRepositoryImpl implements ContactUsRepository   {
     }
 
     @Override
-    public List<ContactUs> selectAllToReview() {
-        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM contact_us ");
+    public List<ContactUs> selectAll() {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM contact_us");
         log.info("{}",mp);
         List<ContactUs> contact=new ArrayList<ContactUs>();
         for (int i=0;i<mp.size();i++){
             ContactUs contactUs=new ContactUs();
             contactUs.setType(mp.get(i).get("type").toString());
             contactUs.setTitle(mp.get(i).get("title").toString());
+            contactUs.setName(mp.get(i).get("name").toString());
+            contactUs.setEmail(mp.get(i).get("email").toString());
+            contactUs.setMobile(mp.get(i).get("mobile").toString());
+            contactUs.setMessage(mp.get(i).get("message").toString());
+            contact.add(contactUs);
+
+        }
+        log.info("{}",contact);
+        return contact;
+    }
+
+    @Override
+    public List<ContactUs> selectAllToReview() {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM contact_us WHERE status=1");
+        log.info("{}",mp);
+        List<ContactUs> contact=new ArrayList<ContactUs>();
+        for (int i=0;i<mp.size();i++){
+            ContactUs contactUs=new ContactUs();
+            contactUs.setType(mp.get(i).get("type").toString());
+            contactUs.setTitle(mp.get(i).get("title").toString());
+            contactUs.setName(mp.get(i).get("name").toString());
+            contactUs.setEmail(mp.get(i).get("email").toString());
+            contactUs.setMobile(mp.get(i).get("mobile").toString());
+            contactUs.setMessage(mp.get(i).get("message").toString());
+            contact.add(contactUs);
+
+        }
+        log.info("{}",contact);
+        return contact;
+    }
+
+    @Override
+    public List<ContactUs> selectAllReviewed() {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM contact_us WHERE status=2");
+        log.info("{}",mp);
+        List<ContactUs> contact=new ArrayList<ContactUs>();
+        for (int i=0;i<mp.size();i++){
+            ContactUs contactUs=new ContactUs();
+            contactUs.setType(mp.get(i).get("type").toString());
+            contactUs.setTitle(mp.get(i).get("title").toString());
+            contactUs.setName(mp.get(i).get("name").toString());
+            contactUs.setEmail(mp.get(i).get("email").toString());
+            contactUs.setMobile(mp.get(i).get("mobile").toString());
+            contactUs.setMessage(mp.get(i).get("message").toString());
+            contact.add(contactUs);
+
+        }
+        log.info("{}",contact);
+        return contact;
+    }
+
+    @Override
+    public List<ContactUs> selectByDate(Date date) {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM contact_us WHERE date=?",date);
+        log.info("{}",mp);
+        List<ContactUs> contact=new ArrayList<ContactUs>();
+        for (int i=0;i<mp.size();i++){
+            ContactUs contactUs=new ContactUs();
+            contactUs.setType(mp.get(i).get("type").toString());
+            contactUs.setTitle(mp.get(i).get("title").toString());
+            contactUs.setName(mp.get(i).get("name").toString());
+            contactUs.setEmail(mp.get(i).get("email").toString());
+            contactUs.setMobile(mp.get(i).get("mobile").toString());
+            contactUs.setMessage(mp.get(i).get("message").toString());
+            contact.add(contactUs);
+
+        }
+        log.info("{}",contact);
+        return contact;    }
+
+    @Override
+    public List<ContactUs> selectByType(String type) {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM contact_us WHERE type=?",type);
+        log.info("{}",mp);
+        List<ContactUs> contact=new ArrayList<ContactUs>();
+        for (int i=0;i<mp.size();i++){
+            ContactUs contactUs=new ContactUs();
+            contactUs.setType(mp.get(i).get("type").toString());
             contactUs.setTitle(mp.get(i).get("title").toString());
             contactUs.setName(mp.get(i).get("name").toString());
             contactUs.setEmail(mp.get(i).get("email").toString());
