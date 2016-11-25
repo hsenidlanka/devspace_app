fb032ec938845db4b044e965564730f672e54955
difnajayashani <difna4@gmail.com>
2016-11-25 14:48:17 +0530

    NameSelectTypehead function added

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/user/UserRepositoryImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/user/UserRepositoryImpl.java
@@ -41,14 +41,14 @@ public  class UserRepositoryImpl implements UserRepository {
         String un = user.getUsername();
         String pw = user.getPassword();
         if (un != "" && pw != "") {
-            // if (checkUsernameUnique(un)==true) {
-            String sql = "INSERT INTO customer " +
-                    "(title,first_name,last_name,username,password,email,address_line1,address_line2,address_line3,mobile,registered_date,status) VALUES (?,?,?,?,sha1(?),?,?,?,?,?,CURRENT_DATE,1)";
+           // if (checkUsernameUnique(un)==true) {
+                String sql = "INSERT INTO customer " +
+                        "(title,first_name,last_name,username,password,email,address_line1,address_line2,address_line3,mobile,registered_date,status) VALUES (?,?,?,?,sha1(?),?,?,?,?,?,CURRENT_DATE,1)";
 
-            row = jdbcTemplate.update(sql, new Object[]{user.getTitle(), user.getFirstName(), user.getLastName(), user.getUsername(), user.getPassword(),
-                    user.getEmail(), user.getAddressL1(), user.getAddressL2(), user.getAddressL3(), user.getMobile()});
+                row = jdbcTemplate.update(sql, new Object[]{user.getTitle(), user.getFirstName(), user.getLastName(), user.getUsername(), user.getPassword(),
+                        user.getEmail(), user.getAddressL1(), user.getAddressL2(), user.getAddressL3(), user.getMobile()});
 
-            log.info("customer inserted");
+                log.info("customer inserted");
           /*  }else
                 log.info("username already available");*/
         } else
@@ -62,7 +62,7 @@ public  class UserRepositoryImpl implements UserRepository {
     @Override
     public int delete(String username) throws IllegalArgumentException {
 
-        // user.setUsername(username);
+       // user.setUsername(username);
         String sql = "DELETE FROM customer WHERE username = ?";
         int row = jdbcTemplate.update(sql, new Object[]{username});
         log.info("deleted");
@@ -83,7 +83,7 @@ public  class UserRepositoryImpl implements UserRepository {
             System.out.println(user.getPassword());
 
             String sql = "UPDATE customer SET password = sha1(?) WHERE username = ? ";
-            row = jdbcTemplate.update(sql, new Object[]{user.getPassword(), user.getUsername()});
+             row = jdbcTemplate.update(sql, new Object[]{user.getPassword(), user.getUsername()});
             log.info(row + "password changed");
         } else log.info("cannot change password");
         return row;
@@ -96,10 +96,10 @@ public  class UserRepositoryImpl implements UserRepository {
 
     /**
      * authenticate username and password matched for a existing customer
-     blocked=2
-     credentials matched=1
-     mismatched=0
-     */
+       blocked=2
+       credentials matched=1
+       mismatched=0
+    */
     @Override
     public int loginAuthenticate(String username,String password) {
 
@@ -152,7 +152,7 @@ public  class UserRepositoryImpl implements UserRepository {
             customer.setAddressL2(mp.get(i).get("address_line2").toString());
             if (mp.get(i).get("address_line3")!=null) {
                 customer.setAddressL3(mp.get(i).get("address_line3").toString());
-            }
+           }
             customer.setMobile(mp.get(i).get("mobile").toString());
             customer.setRegDate(Date.valueOf(mp.get(i).get("registered_date").toString()));
             customer.setStatus(mp.get(i).get("status").toString());
@@ -291,8 +291,8 @@ public  class UserRepositoryImpl implements UserRepository {
 
     /*retrieve details of customers by a given attribute*/
     @Override
-    public List<User> filterByCity(String city) {
-        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM customer WHERE address_line3 = ? AND status='active'",city);
+    public List<User> filterByCity(String city, String status) {
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM customer WHERE address_line3 = ? AND status= ? ",city,status);
         List<User> customerDetails=new ArrayList<User>();
 
         for (int i=0;i<mp.size();i++){
@@ -325,8 +325,8 @@ public  class UserRepositoryImpl implements UserRepository {
     /*retrieve total no.of customers*/
     @Override
     public int countUsers() {
-        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT COUNT(*) FROM customer ");
-        int count = Integer.parseInt(mp.get(0).get("total").toString());
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM customer ");
+        int count = mp.size();
         log.info("{}",count);
         return count;
     }
@@ -534,9 +534,9 @@ public  class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
-    public List<User> selectAllByNameTypeAhead(String nameKey) {
+    public List<User> selectAllByNameTypeAhead(String nameKey, String status) {
         String key = "%" + nameKey + "%";
-        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM customer WHERE username LIKE ?", key);
+        List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT * FROM customer WHERE username LIKE ? AND status = ?", key,status);
         List<User> customerDetails = new ArrayList<User>();
 
         for (int i = 0; i < mp.size(); i++) {
