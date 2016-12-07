14cfc375b50ee8f399ecce56111514d0adef8830
Dineesha <dinsuraw@gmail.com>
2016-12-07 10:51:42 +0530

    Add method to add items to cart

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/shopping_cart/ShoppingCartRepository.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/shopping_cart/ShoppingCartRepository.java
@@ -7,7 +7,7 @@ import hsl.devspace.app.corelogic.domain.ShoppingCart;
  * Created by hsenid on 9/23/16.
  */
 public interface ShoppingCartRepository {
-    int add(ShoppingCart shoppingCart);
+    int addCart(ShoppingCart shoppingCart);
     int delete(int id);
     int countPerCustomer(String customerUsername);
     int countPerGuest(String mobile);
--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/shopping_cart/ShoppingCartRepositoryImpl.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/shopping_cart/ShoppingCartRepositoryImpl.java
@@ -32,13 +32,33 @@ public class ShoppingCartRepositoryImpl implements ShoppingCartRepository {
 
     /*Add new cart*/
     @Override
-    public int add(ShoppingCart shoppingCart) {
+    public int addCart(ShoppingCart shoppingCart) {
         int row;
         String sql = "INSERT INTO shopping_cart " +
                 "(net_cost,customer_id,guest_id) VALUES (?,(SELECT id FROM customer WHERE username=?),(SELECT id FROM guest WHERE mobile=?))";
         row = jdbcTemplate.update(sql, new Object[]{shoppingCart.getNetCost(),shoppingCart.getCustomerUsername(),shoppingCart.getGuestMobile()});
         log.info("{} new shopping cart added",row);
         return row;
+    }
+
+    public void addProductsToCart(List<Map<String, Object>> items) {
+        for (int i = 0; i < items.size(); i++) {
+            String productName = items.get(i).get("itemName").toString();
+            int quantity = Integer.parseInt(items.get(i).get("quantity").toString());
+            List<Map<String, Object>> mp = jdbcTemplate.queryForList("SELECT id FROM item WHERE name=?", productName);
+            int id = Integer.parseInt(mp.get(0).get("id").toString());
+            String type = "item";
+            if (mp.size() == 0) {
+                List<Map<String, Object>> mp1 = jdbcTemplate.queryForList("SELECT id FROM package WHERE name=?", productName);
+                id = Integer.parseInt(mp1.get(0).get("id").toString());
+                type = "package";
+            }
+
+            String sql = "INSERT INTO product " +
+                    "(type,type_id,quantity) VALUES (?,?,?)";
+            jdbcTemplate.update(sql, new Object[]{type, id, quantity});
+        }
+
     }
 
     /*delete a cart*/
