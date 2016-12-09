639d5a35373d3a7e3b32cc809284de74779bbc13
MashiW <maheshiweerasinghe486@gmail.com>
2016-12-09 17:30:19 +0530

    modified item mgt

---
+++ modules/admin/src/main/webapp/themes/hsenid/js/itemPagination.js
@@ -0,0 +1,58 @@
+/*
+ * default pagination function
+ */
+
+$(document).ready(function () {
+
+    /**
+     * ajax function for getting records count
+     */
+    $("#pagination2").hide();
+
+    $.ajax({
+        url: 'PaginationTable',
+        success: function (recCount) {
+            pag.simplePaginator('setTotalPages', Math.ceil(recCount / 10));
+        }
+    })
+
+    var pag = $('#pagination').simplePaginator({
+        // options here
+
+        // the number of total pages
+        totalPages: 7,
+
+        // maximum of visible buttons
+        maxButtonsVisible: 5,
+
+        /*setTotalPages:10,*/
+
+        // page selected
+        currentPage: 1,
+
+        // text labels for buttons
+        nextLabel: 'next',
+        prevLabel: 'prev',
+        firstLabel: 'first',
+        lastLabel: 'last',
+
+        // specify if the paginator click in the currentButton
+        clickCurrentPage: true,
+
+        // called when a page is changed.
+        pageChange: function (page) {
+
+            $.ajax({
+                url: '',
+                dataType: "json",
+                data: {"initPage": page},
+                success: function (data) {
+                    $('#tblItems').bootstrapTable('load', data);
+
+
+                }
+            })
+        }
+    });
+})
+
