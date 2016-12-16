1bdc6096b7b68e91d07d8efc00e0d670529984da
difnajayashani <difna4@gmail.com>
2016-12-16 18:12:23 +0530

    refined

--- modules/admin/src/test/java/hsl/devspace/app/admin/test/usermanagement/UserControllerTest.java
+++ modules/admin/src/test/java/hsl/devspace/app/admin/test/usermanagement/UserControllerTest.java
@@ -1,36 +1,40 @@
 package hsl.devspace.app.admin.test.usermanagement;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.Test;
 
-@Test
-@WebAppConfiguration
-@ContextConfiguration(locations = {"classpath:spring-test-config.xml","classpath:../webapp/WEB-INF/admin-servlet.xml"})
-public class UserControllerTest {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-    private static final Logger LOG = LoggerFactory.getLogger(UserControllerTest.class);
+
+@Test
+@ContextConfiguration(locations = {"classpath:spring-test-config.xml"})
+public class UserControllerTest extends AbstractTestNGSpringContextTests {
+
+    private static final Logger LOG = LogManager.getLogger(UserControllerTest.class);
 
     @Autowired
     private WebApplicationContext wac;
 
     private MockMvc mockMvc;
 
-
     @Test
-    public void addUserTest() {
+    public void addUserTest()  {
         try {
-           this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-       /*     mockMvc.perform(get("/users/add"))
+
+            mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+            mockMvc.perform(get("/users/add"))
                     .andExpect(status().isOk())
-                    .andExpect(view().name("/user_management/userAdd"));*/
+                    .andExpect(view().name("/user_management/userAdd"));
         } catch (Exception e) {
             e.printStackTrace();
             LOG.error("the exception is :",e);
