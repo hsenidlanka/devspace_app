fa9f57d1f8bcff580112af78edc32a356aa195d4
difnajayashani <difna4@gmail.com>
2016-12-27 15:15:51 +0530

    refactored code on user filterings

--- modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/user/UserRepository.java
+++ modules/core-logic/src/main/java/hsl/devspace/app/corelogic/repository/user/UserRepository.java
@@ -14,6 +14,7 @@ import java.util.List;
  */
 public interface UserRepository {
 
+
     public int add(User user) throws SQLIntegrityConstraintViolationException, DuplicateKeyException;
 
     int delete(String username) throws SQLException;
